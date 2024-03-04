776c876f597a43738fa89c72e834f673ba2873eb
Jason Norwood-Young <jason@10layer.com>
2024-03-04 18:27:21 +0100

    Move projects up a level

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -1,4 +1,6 @@
 import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config();
 
-export const PROJECT_DIR = path.resolve('./projects');
+export const PROJECT_DIR = path.resolve(process.env.PROJECT_DIR || 'projects');
 export const FILETYPES = ['pdf', 'txt', 'html'];
